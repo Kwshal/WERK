59b855c074df78ee3c6598c61d2dad8f3e1cbcc9
Kwshal <kushal3738@gmail.com>
2026-06-07 19:32:56 +0530

    .

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 let leavetimeEl = document.getElementById("leavetime-el");
-let leavetime = document.getElementById("leavetime");
+let enterTimeEl = document.getElementById("back-home-time");
+let clockInEl = document.getElementById("clock-in-time");
+let clockOutEl = document.getElementById("clock-out-time");
+
+let timeBtns = document.querySelectorAll(".time-btn");
+
+timeBtns.forEach((btn) => {
+     btn.addEventListener("click", addtime);
+});
+
+function addtime() {
+     let time = new Date();
+     let hours = time.getHours();
+     let minutes = time.getMinutes().toString().padStart(2, "0");
+     let timeString = hours + ":" + minutes;
+     this.parentElement.innerHTML = timeString;
+     console.log(typeof minutes);
+}
